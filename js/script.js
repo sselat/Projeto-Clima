f73f78e780f6d8de7ed3f41481e373308c1d9de2
Tales B. Albuquerque <101683474+sselat@users.noreply.github.com>
2022-09-15 20:41:45 -0400

    commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 // https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=acc1cd1e6f8d51a82d971be585b188b2
+
+const apiKey = "acc1cd1e6f8d51a82d971be585b188b2";
+const apiCountryURL = "https://countryflagsapi.com/png/"
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+const showWeatherData = (city) =>{
+    console.log(city);
+}
+
+searchBtn.addEventListener("click", (e) =>{
+    e.preventDefault();
+
+    const city = cityInput.value;
+
+    showWeatherData(city);
+});
